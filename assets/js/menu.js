bad3144dde16993e27d21ee9f2e1c43133580fc1
firekilin <wl00161839@gmail.com>
2019-05-27 23:02:56 +0800

    Add files via upload

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,7 +3,7 @@
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     var metaTag=document.createElement('meta');
     metaTag.name="viewport";
-    metaTag.content="width='788px', initial-scale=1.0, maximum-scale=1.0, user-scalable=0";
+    metaTag.content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0";
     document.getElementsByTagName('head')[0].appendChild(metaTag);
 }
 function myFunction(x) {
