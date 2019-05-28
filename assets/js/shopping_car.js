f02de85f6ddcc9a5dee95e1a9a6d37c826e3739d
MiaaaQ <47880885+MiaaaQ@users.noreply.github.com>
2019-05-28 14:19:51 +0800

    Add files via upload

--- assets/js/shopping_car.js
+++ assets/js/shopping_car.js
@@ -1,8 +1,29 @@
+count();
 $(document).ready(function()  {
 	$(".up").click(function(){
 	$(this).siblings('input').val(parseInt($(this).siblings('input').val(), 10)+1);
+	count();
 });
 $(".down").click(function(){
 	$(this).siblings('input').val(parseInt($(this).siblings('input').val(), 10)-1);
+	count();
 });
+
+
+
+
 });
+
+function count(){
+	var shopping_total=0;
+$('.shopping_list tr').each(function( index,value){
+var show=0;
+if(index>=1&&index<($('.shopping_list tr').length-1)){
+show=Number($(this).find('td.car_product_price')[0].textContent.substr(1));
+show*=Number($(this).find('input.number').val());
+$(this).find('td.car_product_total')[0].textContent=show;
+}
+shopping_total+=show;
+})
+$('.buy_total')[0].textContent=shopping_total;
+}
